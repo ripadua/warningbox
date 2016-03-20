368acca6566b18c8c204383a566180418ff59d20
ricardocrispim <ripadua7@gmail.com>
2016-03-20 15:24:26 -0300

    Exclusao de produtos

--- www/js/index.js
+++ www/js/index.js
@@ -43,8 +43,8 @@ var app = {
 //            if (estabelecimento == undefined) {
 //                window.location.href='paginas/estabelecimento.html';    
 //            } 
-//            listar();
 //        }
+        listar();
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
@@ -93,7 +93,7 @@ function remover(e) {
         var elem = e.target.parentElement;
         elem.parentNode.removeChild(elem);
         $.ajax({
-            url: 'http://warningbox-ripadua.c9users.io/vencimentos/' + idProduto,
+            url: 'http://warningbox-ripadua.c9users.io/vencimentos/' + idProduto + '.json',
             type: 'DELETE',
             success: function(result) {
                 alert('Produto removido com sucesso.');
